4a6863a5a04aff568b498edd6b95a8b63ed0a3b2
Dmitry Shevchenko <thisninja@yandex.ru>
2015-02-04 19:49:01 +0200

    Added a little bit more functional

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ exports.startServer = function(config, callback) {
 
     var routeOptions = {
         reload: config.liveReload.enabled,
-        test: {a: 1, b: 2},
         LangObj1: {
             title: 'English'
         },
@@ -33,11 +32,9 @@ exports.startServer = function(config, callback) {
         path: '/',
         handler: function(req, reply) {
             request('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=trnsl.1.1.20150120T002528Z.2126c945e0033cc4.29d883edef031620bee00dd72e3d1419a6ee274b&ui=en').then(function(data) {
-                routeOptions.data = data.toString();
-                var newData = JSON.parse(data);
-                console.log(newData.langs);
-                routeOptions.langs = newData.langs;
-                console.log(typeof routeOptions.langs);
+                routeOptions.data = JSON.parse(data);
+                routeOptions.langs = JSON.parse(data).langs;
+                routeOptions.dirs = JSON.parse(data).dirs;
                     reply.view('index', routeOptions);
             });
 
